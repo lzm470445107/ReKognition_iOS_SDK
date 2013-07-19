9e7bebe7bb6435cd66dfb84f90e14e4492f35dad
cys <coralcys@yushanc.gateway.2wire.net>
2013-07-18 22:13:49 -0700

    Initial Commit

---
+++ Rekognition_iOS_SDK/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Rekognition_iOS_SDK
+//
+//  Created by cys on 7/18/13.
+//  Copyright (c) 2013 Orbeus Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Rekognition_iOS_SDK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Rekognition_iOS_SDK
+//
+//  Created by cys on 7/18/13.
+//  Copyright (c) 2013 Orbeus Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
